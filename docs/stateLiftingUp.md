6c0a3368c06c3895cdfb950795b4fa23f5590413
JuniorMSG <milk6645@gmail.com>
2024-04-07 21:29:08 +0900

    [240407 TIL] Test_Statue Componset

--- docs/stateLiftingUp.md
+++ docs/stateLiftingUp.md
@@ -1,1 +1,20 @@
 ## stateLiftingUp (상태 끌어 올리기)
+
+## state
+
+- 상태는 컴포넌트의 데이터를 의미한다.
+- 자신의 출력값을 변경
+
+## 유의사항 => state 를 직접 수정하지 마세요
+
+- state 를 직접 수정하면 안된다.
+- setState() 함수를 사용하여 state 를 변경해야 한다.
+
+## Class component
+
+State LifeCycle
+
+## Functional component
+
+훅으로 관리한다.
+
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Extraction from "./components/props/Extraction";
 import Composition from "./components/props/Composition";
+import StateClassComponent from "./components/StateClassComponent";
+import FunctionalComponent from "./components/FunctionalComponent";
 
 
 function App() {
   return (
     <div className="App">
+        <FunctionalComponent />
+        <StateClassComponent />
         <Extraction />
         {/*<Composition />*/}
     </div>
